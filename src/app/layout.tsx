a29f04e74373fce1485956fe3de8c5913a21b4f3
prayertimes <prayertimes@proton.me>
2023-10-29 12:43:57 +0300

    add corejs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "core-js/full/global-this";
 
 const inter = Inter({ subsets: ["latin"] });
 
